1e7426b876604f158588b7b9c6765d6b8e910362
Jillian Amelung <jpamelung@gmail.com>
2021-08-16 19:23:59 -0400

    working on models

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -41,17 +41,16 @@ const thoughtController = {
       },
 
       // update thought by id
-  updateThought({ params, body }, res) {
+  updateThought({ params, body}, res) {
     Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
-      .then(dbThoughtData => {
-        if (!dbThoughtData) {
-          res.status(404).json({ message: 'No thought found with this id!' });
-          return;
-        }
-        res.json(dbThoughtData);
-      })
-      .catch(err => res.json(err));
-  },
+      .populate({ path: 'reactions', select: '__v' })
+      .select('__v')
+      .then (dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+      },
 
   // delete thought
   deleteThought({ params }, res) {
